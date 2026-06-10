27676c4e8113c677a87a88e8dbf38ea393bf4c30
jaagam-bit <jaagam3546@gmail.com>
2026-06-10 16:02:30 +0530

    Day6 Submit

---
+++ Day-6/Ques24.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,x,result =1;
+    printf("Enter base (x): ");
+    scanf("%d",&x);
+    printf("Enter number (n): ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        result*=x;
+    }
+     printf("%d^%d = %d\n",x,n,result);
+    return 0;
+}
