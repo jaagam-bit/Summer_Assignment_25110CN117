7bde487c01f84ff18a21f07e080adbc698b378c9
jaagam-bit <jaagam3546@gmail.com>
2026-06-12 18:06:26 +0530

    Day10 Submit

---
+++ Day-10/Ques40.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int rows,i,j,space;
+    printf("Enter number of rows:");
+    scanf("%d",&rows);
+    for(i=1;i<=rows;i++)
+    {
+        for(space=1;space<=rows-i;space++)
+    {
+        printf(" ");
+    }
+    for(j=0;j<i;j++)
+    {
+        printf("%c",'A'+j);
+    }
+    for(j=i-2;j>=0;j--)
+    {
+        printf("%c",'A'+j);
+    }
+    printf("\n");
+    }
+    return 0;
+}
