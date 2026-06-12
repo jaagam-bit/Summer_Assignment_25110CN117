57be8f24a522d517354d90dc08986ffb5ea68774
jaagam-bit <jaagam3546@gmail.com>
2026-06-12 17:40:54 +0530

    Day9 Submit

---
+++ Day-9/Ques36.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int size,i,j;
+    printf("Enter size of square:");
+    scanf("%d",&size);
+    for(i=1;i<=size;i++)
+    {
+        for(j=1;j<=size;j++)
+        {
+            if(i==1||i==size||j==1||j==size)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
