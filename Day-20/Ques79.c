70db9d94fad287a0b0419f9b937c36e3d9c172c3
jaagam-bit <jaagam3546@gmail.com>
2026-06-20 16:32:06 +0530

    Day20 Submit

---
+++ Day-20/Ques79.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int r,c,i,j;
+    printf("Enter rows,columns: ");
+    scanf("%d %d",&r,&c);
+    int a[r][c];
+    printf("Enter element of matrix: ");
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+    for(i=0;i<r;i++) {
+        int sum = 0;
+        for(j=0;j<c;j++)
+            sum += a[i][j];
+        printf("Sum of row %d = %d\n", i + 1, sum);
+    }
+    return 0;
+}
