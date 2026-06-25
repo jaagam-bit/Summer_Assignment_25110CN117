d142e2cca80f597c7ed13324f997c13a033c48e1
jaagam-bit <jaagam3546@gmail.com>
2026-06-26 00:09:12 +0530

    Day25 Submit

---
+++ Day-25/Ques99.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int n;
+    printf("Enter number of names: ");
+    scanf("%d",&n);
+    char names[n][50],temp[50];
+    for(int i=0;i<n;i++)
+    scanf("%s",names[i]);
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(strcmp(names[i],names[j])>0)
+            {
+                strcpy(temp,names[i]);
+                strcpy(names[i],names[j]);
+                strcpy(names[j],temp);
+            }
+        }
+    }
+    printf("Names in alphabetical order:");
+    for(int i=0;i<n;i++)
+        printf("%s\n",names[i]);
+        return 0;
+}
